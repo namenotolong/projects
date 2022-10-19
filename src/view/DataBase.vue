c5c8b00b79af8b47a16ce07f6d99e0565b563927
namenotolong <525691990@qq.com>
2022-10-19 23:30:43 +0800

    fix

--- src/view/DataBase.vue
+++ src/view/DataBase.vue
@@ -251,9 +251,21 @@ export default {
             }
         }
     },
+    destroyed() {
+        window.onbeforeunload = null
+    },
 
     mounted() {
         this.listTypes();
+        window.onbeforeunload = function (e) {
+            e = e || window.event;
+            // 兼容IE8和Firefox 4之前的版本
+            if (e) {
+                e.returnValue = '关闭提示';
+            }
+            // Chrome, Safari, Firefox 4+, Opera 12+ , IE 9+
+            return '关闭提示';
+        }
     },
 
     methods: {
